eb7d9bc57ef7407ea0feafb08f9aa72e717268db
AGulev <me@agulev.com>
2026-05-27 22:09:29 +0200

    Defold 1.13.0 update: remove old APIs

--- game/ground.go
+++ game/ground.go
@@ -1,28 +1,21 @@
 embedded_components {
   id: "ground1"
   type: "sprite"
-  data: "tile_set: \"/assets/game.atlas\"\n"
-  "default_animation: \"ground\"\n"
+  data: "default_animation: \"ground\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/game.atlas\"\n"
+  "}\n"
   ""
   position {
-    x: 0.0
     y: 6.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
   }
 }
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
-  data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
@@ -32,15 +25,9 @@ embedded_components {
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: 0.0\n"
   "      y: 15.0\n"
-  "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "      w: 1.0\n"
   "    }\n"
   "    index: 0\n"
   "    count: 3\n"
@@ -49,99 +36,65 @@ embedded_components {
   "  data: 15.0\n"
   "  data: 10.0\n"
   "}\n"
-  "linear_damping: 0.0\n"
-  "angular_damping: 0.0\n"
-  "locked_rotation: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "signleft"
   type: "sprite"
-  data: "tile_set: \"/assets/game.atlas\"\n"
-  "default_animation: \"signLeft\"\n"
+  data: "default_animation: \"signLeft\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/game.atlas\"\n"
+  "}\n"
   ""
   position {
     x: 354.0
     y: 60.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
   }
 }
 embedded_components {
   id: "ground2"
   type: "sprite"
-  data: "tile_set: \"/assets/game.atlas\"\n"
-  "default_animation: \"ground\"\n"
+  data: "default_animation: \"ground\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/game.atlas\"\n"
+  "}\n"
   ""
   position {
     x: -800.0
     y: 6.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
   }
 }
 embedded_components {
   id: "ground3"
   type: "sprite"
-  data: "tile_set: \"/assets/game.atlas\"\n"
-  "default_animation: \"ground\"\n"
+  data: "default_animation: \"ground\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/game.atlas\"\n"
+  "}\n"
   ""
   position {
     x: 800.0
     y: 6.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
   }
 }
 embedded_components {
   id: "signright"
   type: "sprite"
-  data: "tile_set: \"/assets/game.atlas\"\n"
-  "default_animation: \"signRight\"\n"
+  data: "default_animation: \"signRight\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/game.atlas\"\n"
+  "}\n"
   ""
   position {
     x: -354.0
     y: 60.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
   }
 }
